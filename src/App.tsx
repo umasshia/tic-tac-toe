99e27f0654b9e38eec43065e3409a5cbb2ad0de3
umasshia <giorgisamu.gs@gmail.com>
2022-12-15 00:31:11 -0500

    efficiency update again

--- src/App.tsx
+++ src/App.tsx
@@ -3,62 +3,60 @@ import './App.css';
 
 function App() {
 
-  const [turn, setTurn] = useState('X');
-  const [grid, setGrid] = useState([
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ]);
-  const [winner, setWinner] = useState('');  
+  const [turn, setTurn] = useState<string>('X');
+  const [grid, setGrid] = useState(Array(9).fill(''));
+  const [winner, setWinner] = useState<string>('');  
+  const checkLines = [
+    [[1, 2], [4, 8], [3, 6]],
+    [[0, 2], [4, 7]],
+    [[0, 1], [4, 6], [5, 8]],
+    [[4, 5], [0, 6]],
+    [[3, 5], [0, 8], [2, 6], [1, 7]],
+    [[3, 4], [2, 8]],
+    [[7, 8], [2, 4], [0, 3]],
+    [[6, 8], [1, 4]],
+    [[6, 7], [0, 4], [2, 5]]
+];
 
-  function checkWinner(cells: string[][], rowIndex: number, colIndex: number, trn: string) {
-    let col = 0
-    let row = 0
-    let dg = 0
-    let rdg = 0
-    for (let i = 0; i < 3; i++){
-      cells[rowIndex][i] === trn && col++ 
-      cells[i][colIndex] === trn && row++
-      cells[i][i] === trn && dg++
-      cells[i][3-i] === trn && rdg++
+  function checkWinner(cells: string[][], index: number, trn: string) {
+    var player = cells[index];
+    for (var i = 0; i < checkLines[index].length; i++) {
+        var line = checkLines[index][i];
+        if(player === cells[line[0]] && player === cells[line[1]]) {
+          alert(`${trn} WON!`);
+          setWinner(trn)
+        }
     }
-    if(col === 3 || row === 3 || dg === 3 || rdg === 3) alert(`${trn} Wins!`)
   }
 
-  function handleClick(rowIndex: number, colIndex: number) {
-    if(grid[rowIndex][colIndex] !== ""){
+  function handleClick(index: number) {
+    if(grid[index] !== ""){
       return;
     }
     let cells = [...grid]
     let trn = turn
     if(turn === 'X'){
-      cells[rowIndex][colIndex] = 'X';
+      cells[index] = 'X';
       setTurn('O')
     }
     else{
-      cells[rowIndex][colIndex] = 'O'
+      cells[index] = 'O'
       setTurn('X')
     }
     setGrid(cells)
-    checkWinner(cells, rowIndex, colIndex, trn)
+    checkWinner(cells, index, trn)
   }
 
-  function handleRestart(){
-    setGrid([
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
-    ])
+  function handleRestart() {
+    setGrid(Array(9).fill(''));
     setWinner('')
   }
 
   return (
     <div className="App">
       <div className='grid'>
-        {grid.map((row, rowIndex) => (
-          row.map((cell, colIndex) => (
-            <div key={rowIndex + colIndex} className={'cell ' + (winner  ? 'done' : '')} onClick={() => handleClick(rowIndex, colIndex)} >{cell}</div>
-          ))
+        {grid.map((cell, index) => (
+            <div key={index} className={'cell ' + (winner  ? 'done' : '')} onClick={() => handleClick(index)} >{cell}</div>
         ))}
       </div>
       <div className ='restart' onClick={handleRestart}>Restart</div>
